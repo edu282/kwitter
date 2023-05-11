9510e0f30b7002350b735014894481bbee89cbe5
edu282 <91227097+edu282@users.noreply.github.com>
2023-05-11 16:46:30 -0300

    Add files via upload

--- kwitterPage.js
+++ kwitterPage.js
@@ -27,45 +27,51 @@ function send()
 /* Aula 96 termina aqui */
 
 /* Inicio da aula 97 */
-function () /* Chamar a função getData */
+function getData() 
           { firebase.database().ref("/"+roomName).on('value', function(snapshot) 
             { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) 
                { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") 
                {
-          = childKey; /* Escreva a variável firebaseMessageId para conter todas as Ids únicas, das mensagens, geradas pelo firebase */
-          = childData; /* Escreva a variável messageData para conter todas as mensagens, likes e nomes de usuário para todas as mensagens */
+                firebaseMessageId = childKey; 
+                messageData = childData; 
 //Início do código
-          console.log(); /* Escreva a variável firebaseMessageId para ser verificada no console */
-          console.log(); /* Escreva a variável messageData para ser verificada no console */
+          console.log(firebaseMessageId);
+          console.log(messageData);
 
-          nome = messageData[]; /* Escreva a chave 'name' que irá buscar o valore do nome */ 
-          message = messageData[]; /* Escreva a chave 'message' que irá buscar o valore da mensagem */ 
-          like = messageData[];  /* Escreva a chave 'like' que irá buscar o número de likes para a mensagem */
+          nome = messageData['name']; 
+          message = messageData['message']; 
+          like = messageData['like'];  
 
           nameWithTag = "<h4> "+ nome +"<img class='user_tick' src='tick.png'></h4>";
           messageWithTag = "<h4 class='message_h4'>" + message + "</h4>";
           like_button ="<button class='btn btn-warning' id="+firebaseMessageId+" value="+like+" onclick='updateLike(this.id)'>";
           spanWithTag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
 
-        row = ; /* Coloque todas as variáveis acima dentro da variável row */      
-        document.getElementById("").innerHTML += row; /* Chamar o id output */
+        row = nameWithTag + messageWithTag + like_button + spanWithTag ; 
+        document.getElementById("output").innerHTML += row; 
 
 //Fim do código
       } });  }); }
 getData();
 
-function (messageId) /* Chamar a função updateLike. *Importante: o messageId é a identificação única da mensagem no banco de dados. */
+function updateLike(messageId) /*  *Importante: o messageId é a identificação única da mensagem no banco de dados. */
 {
   console.log("botão de like pressionado - " + messageId);
-	buttonId = ; /* Atribua o valor messageId a nova variável buttonId */
-	likes = document.getElementById().value; /* Chamar a variável buttonId */
-	updatedLikes = Number(likes) ; /* Incrementar em + 1 o número de likes e armazenar na variável updatedLikes. */
-	console.log(); /* Adicionar a variável updatedLikes que armazena o valor de likes incrementado */
+	buttonId = messageId ; 
+	likes = document.getElementById(buttonId).value; 
+	updatedLikes = Number(likes) + 1 ; 
+	console.log(updatedLikes); 
 
-	firebase.database().ref().child().update({ /* Chamar o roomName para a ref e o messageId para o child */
-		like :  /* Chamar a variável updatedLikes (que é o valor incrementade de like) na chave like. */ 
+	firebase.database().ref(roomName).child(messageId).update({ 
+		like : updatedLikes 
 	 });
 
 }
 
 /* Adicionar a função logout que será a mesma presente no arquivo kwitterRomm.js */
+function logout() 
+{
+localStorage.removeItem("userName"); 
+localStorage.removeItem("roomName"); 
+    window.location = "index.html"; 
+}
